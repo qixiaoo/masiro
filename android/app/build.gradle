f4d5bddf79260e8ead014b7c609f1f64dbc0388e
qixiao <nine.qin@qq.com>
2024-09-01 22:31:17 +0800

    ci: build android application

--- android/app/build.gradle
+++ android/app/build.gradle
@@ -28,6 +28,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        minSdkVersion flutter.minSdkVersion
     }
 
     buildTypes {
