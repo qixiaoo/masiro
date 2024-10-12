d56a2403858d5d2ff87cbf300110aff5b357f829
qixiao <nine.qin@qq.com>
2024-10-12 21:22:01 +0800

    feat(ui): update licenses

--- lib/misc/oss_licenses.dart
+++ lib/misc/oss_licenses.dart
@@ -113,6 +113,7 @@ const allDependencies = <Package>[
   _recase,
   _rxdart,
   _screen_retriever,
+  _scrollable_positioned_list,
   _shared_preferences,
   _shared_preferences_android,
   _shared_preferences_foundation,
@@ -185,6 +186,7 @@ const dependencies = <Package>[
   _path_provider,
   _puppeteer,
   _rxdart,
+  _scrollable_positioned_list,
   _shared_preferences,
   _tray_manager,
   _webview_cookie_manager,
@@ -6319,6 +6321,47 @@ SOFTWARE.''',
   isMarkdown: false,
   isSdk: false,
   dependencies: [PackageRef('flutter')],
+);
+
+/// scrollable_positioned_list 0.3.8
+const _scrollable_positioned_list = Package(
+  name: 'scrollable_positioned_list',
+  description:
+      '''A list with helper methods to programmatically scroll to an item.
+''',
+  homepage:
+      'https://github.com/google/flutter.widgets/tree/master/packages/scrollable_positioned_list',
+  authors: [],
+  version: '0.3.8',
+  license: '''Copyright 2018 the Dart project authors, Inc. All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of Google Inc. nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.''',
+  isMarkdown: false,
+  isSdk: false,
+  dependencies: [PackageRef('flutter'), PackageRef('collection')],
 );
 
 /// shared_preferences 2.3.2
